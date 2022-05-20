b5bfc70a5f10bc6b13b234ddaa186ada047723de
ivan <dfeqcn11@gmail.com>
2022-05-20 21:53:15 +0400

    Added comments to quick sort

--- quick-sort/main.cpp
+++ quick-sort/main.cpp
@@ -7,18 +7,24 @@ using namespace std;
 template <typename T>
 void quickSort(T *array, size_t left, size_t right)
 {
+    //Пока указатели не пересекли друг друга
     if (left < right)
     {
+        //Ищем центральный элемент
         size_t mid = array[(left + right) / 2];
 
         size_t i = left;
         size_t j = right;
+        //Начинаем сдвигать границы массива
         while (i <= j)
         {
+            //Если нашли не тот элемент останавливаем границы
             while (array[i] < mid)
                 i++;
             while (array[j] > mid)
                 j--;
+            
+            //Меням два элемента местами относительно среднего элемента
             if (i <= j)
                 swap(array[i++], array[j--]);
         }
@@ -31,6 +37,7 @@ void quickSort(T *array, size_t left, size_t right)
 int main()
 {
     Random R(1, 200);
+    //Размер массива
     const int n = 20;
     int *array = R.GetArrayInt(n);
 
@@ -43,6 +50,7 @@ int main()
     cout << "]" << endl;
 
     //Сортируем
+    //Начинаем с первого и последнего элемента
     quickSort(array, 0, n - 1);
 
     cout << "Отсортированный массив:" << endl;
