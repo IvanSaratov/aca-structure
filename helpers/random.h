d18d80dffdaf9befd2c8eea59a80a0e6ff66385c
unknown <D:\BAT MAILS>
2022-11-25 03:44:20 +0400

    Aaded comments to helpers function

--- helpers/random.h
+++ helpers/random.h
@@ -5,20 +5,35 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+/**
+ * Доплнительный класс для рандомизации и заполнения массива случайными данными
+ * 
+ * Работает только с векторами
+ * Принимает в себя int и double значения
+ */
 class Random
 {
 private:
     double min, max;
 
 public:
+    /**
+     * Стандартный конструктор, по умолчанию устанавливает границы в диапазоне от 0 до 100
+     */
     Random()
     {
         srand(time(NULL));
 
         this->max = 100;
-        this->min = 0;
+          this->min = 0;
     }
 
+    /**
+	 *   Конструктор double значений
+     *
+     *   @param min нижняя граница
+	 *	 @param max верхняя граница
+     */
     Random(double min, double max)
     {
         srand(time(NULL));
@@ -35,6 +50,12 @@ public:
         }
     }
 
+    /**
+     * Конструктор для int
+     * 
+     * @param min нижняя граница
+     * @param max верхняя граница
+    */
     Random(int min, int max)
     {
         srand(time(NULL));
@@ -53,42 +74,91 @@ public:
 
     ~Random() {}
 
+    /**
+     * Функция задает значения для границ
+     * 
+     * Работает с double
+     * 
+     * @param min нижняя граница
+     * @param max верхняя граница
+    */
     void SetMinMax(double min, double max)
     {
         this->min = min;
         this->max = max;
     }
 
+    /**
+     * @return возвращает нижнюю границу
+    */
     double GetMin()
     {
         return this->min;
     }
 
+    /**
+     * @return возвращает верхнюю границу
+    */
     double GetMax()
     {
         return this->max;
     }
 
+    /**
+     * Функция генерации произвольного Int числа
+     * 
+     * @param min нижняя граница
+     * @param max верхняя граница
+     * 
+     * @return случайно int число
+    */
     int NextInt(int min, int max)
     {
         return min + rand() % (max - min + 1);
     }
 
+    /**
+     * Функция генерации произвольного Int числа от заданных в классе границ
+     * 
+     * @return случайно int число
+    */
     int NextInt()
     {
         return NextInt((int)this->min, (int)this->max);
     }
 
+    /**
+     * Функция генерации произвольного Long числа
+     * 
+     * @param min нижняя граница
+     * @param max верхняя граница
+     * 
+     * @return возвращает случайное Long число
+    */
     size_t NextLong(size_t min, size_t max)
     {
         return min + rand() % (max - min + 1);
     }
 
+    /**
+     * Функция генерации произвольного Long числа от заданных в классе границ
+     * 
+     * @return возвращает случайное Long число
+    */
     size_t NextLong()
     {
         return NextLong((size_t)this->min, (size_t)this->max);
     }
 
+    /**
+     * Функция генерации произвольного Double числа
+     * 
+     * @param min нижняя граница
+     * @param max верхняя граница
+     * @param precision точность генерации после запятой
+     * 
+     * @return возвращает случайное Double число
+    */
     double NextDouble(double min, double max, int precision = 2)
     {
         double value;
@@ -99,11 +169,25 @@ public:
         return value;
     }
 
+    /**
+     * Функция генерации произвольного Double числа от заданных в классе границ
+     * 
+     * @param precision точность генерации после запятой
+     * 
+     * @return возвращает случайное Double число
+    */
     double NextDouble(int precision = 2)
     {
         return NextDouble(this->min, this->max, precision);
     }
 
+    /**
+     * Создает случайны целочисленный массив в памяти и возвращает на него указатель
+     * 
+     * @param count размер массива
+     * 
+     * @return возвращает указатель на массив в памяти
+    */
     int *GetArrayInt(int count = 10)
     {
         int *A = new int[count];
@@ -115,6 +199,14 @@ public:
         return A;
     }
 
+    /**
+     * Создает случайны вещественный массив в памяти и возвращает на него указатель
+     * 
+     * @param count размер массива
+     * @param precision точность после запятой
+     * 
+     * @return возвращает указатель на массив в памяти
+    */
     double *GetArrayDouble(int count = 10, int precision = 2)
     {
         double *A = new double[count];
@@ -126,6 +218,13 @@ public:
         return A;
     }
 
+    /**
+     * Создает вектор целочисленных значений Long и возвращает на него указатель
+     * 
+     * @param count размер вектора
+     * 
+     * @return возвращает указатель на вектор
+    */
     std::vector<size_t> *GetVectorLong(size_t count = 10)
     {
         std::vector<size_t> *A = new std::vector<size_t>(count);
