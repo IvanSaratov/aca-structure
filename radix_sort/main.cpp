fb15fb263d1b371e98ae7650306b2ff0c1c4dc9e
unknown <D:\BAT MAILS>
2022-11-25 04:16:35 +0400

    Added radix sort

--- radix_sort/main.cpp
+++ radix_sort/main.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include "../helpers/random.h"
+#include <Windows.h>
+#include <cstdio>
 
 using namespace std;
 
 template<typename T>
-void radix_sort(vector<T> &array) {
+void radix_sort(vector<T>& array) {
     //Находим максимальный элемент в массиве
     T max = array[0];
     for (size_t i = 0; i < array.size(); i++) {
@@ -25,9 +27,10 @@ void radix_sort(vector<T> &array) {
 
     vector<T> sizes(tmp.size());
 
+    // Сортируем по разрядам числа 123 - три разряда
     for (size_t divider = 1; divider <= max; divider *= 10) {
-        for (size_t i = 0; i < array.size(); i++) {
-            size_t no = array[i] / divider % 10;
+        for (size_t i = 0; i < array.size(); i++) {     // Проходимся по всему массиву
+            size_t no = array[i] / divider % 10;    // Находим первую цифру
             tmp[no][sizes[no]++] = array[i];
         }
 
@@ -43,25 +46,35 @@ void radix_sort(vector<T> &array) {
 
 int main()
 {
-    Random R(1, 200);
-    vector<size_t> *array = R.GetVectorLong(15);
+    SetConsoleOutputCP(1251);   // Устанавливаем кодировку для windows
 
+    Random R(1, 50);
+    vector<size_t>* array = R.GetVectorLong(15);    // Вектор случайны целых чисел
+
+    // Выводи начальный массив
     cout << "Начальный массив:" << endl;
-    cout << "[ ";
+    cout << "[";
     for (size_t i = 0; i < (*array).size(); i++)
     {
-        cout << (*array)[i] << ", ";
+        cout << (*array)[i];
+        if (i != (*array).size() - 1) {
+            cout << ", ";
+        }
     }
     cout << "]" << endl;
 
-    //Сортируем
+    // Сортируем
     radix_sort(*array);
 
+    // Выводим отсортированный массив
     cout << "Отсортированный массив:" << endl;
-    cout << "[ ";
+    cout << "[";
     for (size_t i = 0; i < (*array).size(); i++)
     {
-        cout << (*array)[i] << ", ";
+        cout << (*array)[i];
+        if (i != (*array).size() - 1) {
+            cout << ", ";
+        }
     }
     cout << "]" << endl;
 
